f32d990a24c67576d6ed83bfa02c9628b6082231
Simon Robertson <simon.robertson@shift.online>
2022-08-07 16:35:56 +0100

    Refactors the data module

--- engine/data.js
+++ engine/data.js
@@ -4,18 +4,17 @@ const resourceCache = new Map()
 /**
  * @param {string} path
  */
-export const load = (path) => {
+const load = async (path) => {
     const resource = `/data/${path}.json`
 
     if (resourceCache.has(resource)) {
-        return Promise.resolve(resourceCache.get(resource))
+        return resourceCache.get(resource)
     }
 
     return fetch(resource)
         .then((response) => response.json())
         .then((data) => {
             resourceCache.set(resource, data)
-
             return data
         })
 }
