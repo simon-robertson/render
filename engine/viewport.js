f8ff608dfefb95feb1cb914e27c06a6b6d44731d
Simon Robertson <simon.robertson@shift.online>
2022-08-07 16:36:26 +0100

    Updates the viewport module

--- engine/viewport.js
+++ engine/viewport.js
@@ -1,31 +1,26 @@
 /** @type {HTMLCanvasElement} */
 let canvas
 
-/** @type {WebGL2RenderingContext} */
-let canvasContext
-
 /** */
-export const initializeViewport = async () => {
-    canvas = document.querySelector('#viewport')
+const synchronizeCanvasSize = () => {
+    canvas.width = canvas.offsetWidth
+    canvas.height = canvas.offsetHeight
+}
+
+/**
+ * @param {Window} window
+ */
+export const initializeViewport = async (window) => {
+    canvas = window.document.querySelector('#viewport')
 
     if (canvas === null) {
         throw new Error('Viewport canvas not found')
     }
 
-    canvasContext = canvas.getContext('webgl2', {
-        alpha: false,
-        depth: true,
-        stencil: true,
-        antialias: false,
-        desynchronized: true,
-        premultipliedAlpha: false,
-        preserveDrawingBuffer: false,
-        powerPreference: 'high-performance'
-    })
+    synchronizeCanvasSize()
 
-    if (canvasContext === null) {
-        throw new Error('Failed to create the WebGL2 rendering context')
-    }
-
-    return
+    window.addEventListener('resize', synchronizeCanvasSize, { passive: true })
 }
+
+/** */
+export const getViewportCanvas = () => canvas
