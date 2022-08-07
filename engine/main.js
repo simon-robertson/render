63e1d4ee237305a5620a4e6f3e316894853cfcd1
Simon Robertson <simon.robertson@shift.online>
2022-08-07 16:36:42 +0100

    Updates the main module

--- engine/main.js
+++ engine/main.js
@@ -1,1 +1,11 @@
-console.debug('hello')
+import { initializeRenderer } from './renderer.js'
+import { getViewportCanvas, initializeViewport } from './viewport.js'
+
+/** */
+const main = async () => {
+    // The initialisation order is important here.
+    await initializeViewport(window)
+    await initializeRenderer(getViewportCanvas())
+}
+
+main()
